e204827c58780bc73bc1a4c80b12d0a4036fdb20
AlexKingsland <alex.w.king@hotmail.com>
2024-10-13 17:35:30 -0700

    [fix] append headers to fix cors issue

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from config import SecretsConfig
 from database.models import db
 from api import subscribe_activity
 from flask_cors import CORS
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(SecretsConfig)
+app = Flask(__name__)
+app.config.from_object(SecretsConfig)
 
+def init_app():
     CORS(app, support_credentials=True) # Allow cross origin calls
 
     db.init_app(app)
@@ -19,6 +19,14 @@ def create_app():
     app.register_blueprint(subscribe_activity.subscribe_bp)
     return app
 
+@app.before_request
+def before_request():
+    headers = {'Access-Control-Allow-Origin': '*',
+               'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+               'Access-Control-Allow-Headers': 'Content-Type'}
+    if request.method.lower() == 'options':
+        return jsonify(headers), 200
+
 if __name__ == '__main__':
-    app = create_app()
+    init_app()
     app.run(debug=True)
