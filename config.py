3c04a27b57145bcf6bef4fb6770064cc0324940b
AlexKingsland <alex.w.king@hotmail.com>
2024-10-13 19:49:33 -0700

    fix attempt #76283482394

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class SecretsConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'invalid_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'invalid_db_uri'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://") or 'invalid_db_uri'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ValidationConfig:
